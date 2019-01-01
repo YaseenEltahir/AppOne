653680e6e88bef5a56beaa4db4a0748e010f02f8
YaseenEltahir <yaseeneltahir@gmail.com>
2019-01-01 11:04:56 +0200

    Creating DB.

--- app/src/main/java/com/example/yaseen/myapplication/EssayActivity.java
+++ app/src/main/java/com/example/yaseen/myapplication/EssayActivity.java
@@ -2,6 +2,7 @@ package com.example.yaseen.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -10,12 +11,17 @@ import java.io.InputStreamReader;
 
 public class EssayActivity extends AppCompatActivity {
 
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_essay);
-        TextView essayTextView=findViewById(R.id.essayTextView);
-        essayTextView.setText(readFile("text.txt"));
+        webView=findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/html_sample.html");
+
+//        TextView essayTextView=findViewById(R.id.essayTextView);
+//        essayTextView.setText(readFile("text.txt"));
+//        essayTextView.setText(readFile("Sample.rtf"));
 
     }
     private String readFile(String fileName){
